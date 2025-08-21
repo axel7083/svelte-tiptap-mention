6e8b730c474c57c5fecf71117c174c6d94277dd2
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-08-21 10:12:07 +0200

    feat: make base configurable
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+function getBase(): string | undefined {
+    if(!process.env.GITHUB_REPOSITORY) return;
+    return process.env.GITHUB_REPOSITORY.split('/')[1];
+}
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte({
-      emitCss: true,
-  })],
+    plugins: [svelte({
+        emitCss: true,
+    })],
+    base: getBase(),
 })
